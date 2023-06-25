24f48317baf4f02cad3d63afaab8c93e3adcdd8e
edengugsa <eden12gg@gmail.com>
2023-06-25 19:47:27 -0400

    deleted unnecessary files and implemented profile button on main page

---
+++ App12/Main Screen /MainScreenProgressIndicator.swift
@@ -0,0 +1,22 @@
+//
+//  MainScreenProgressIndicator.swift
+//  App12
+//
+//  Created by Eden Gugsa on 6/25/23.
+//
+
+import Foundation
+
+extension ViewController:ProgressSpinnerDelegate{
+    func showActivityIndicator(){
+        addChild(childProgressView)
+        view.addSubview(childProgressView.view)
+        childProgressView.didMove(toParent: self)
+    }
+    
+    func hideActivityIndicator(){
+        childProgressView.willMove(toParent: nil)
+        childProgressView.view.removeFromSuperview()
+        childProgressView.removeFromParent()
+    }
+}
--- App12/Main Screen /ViewController.swift
+++ App12/Main Screen /ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
 
     let mainScreen = MainScreenView()
     
-//    var contactsList = [Contact]()
-    
+    let childProgressView = ProgressSpinnerViewController()
+        
     var handleAuth: AuthStateDidChangeListenerHandle?
     
     var currentUser:FirebaseAuth.User?
@@ -33,13 +33,9 @@ class ViewController: UIViewController {
             if user == nil{
                 //MARK: not signed in...
                 self.currentUser = nil
-                self.mainScreen.labelText.text = "Please sign in to see the notes!"
-                self.mainScreen.floatingButtonAddContact.isEnabled = false
-                self.mainScreen.floatingButtonAddContact.isHidden = true
-                
-//                //MARK: Reset tableView...
-//                self.contactsList.removeAll()
-//                self.mainScreen.tableViewContacts.reloadData()
+                self.mainScreen.labelText.text = "Please sign in to see your health information!"
+                self.mainScreen.seeProfileButton.isEnabled = false
+                self.mainScreen.seeProfileButton.isHidden = true
                 
                 //MARK: Sign in bar button...
                 self.setupRightBarButton(isLoggedin: false)
@@ -48,31 +44,15 @@ class ViewController: UIViewController {
                 //MARK: the user is signed in...
                 self.currentUser = user
                 self.mainScreen.labelText.text = "Welcome \(user?.displayName ?? "Anonymous")!"
-                self.mainScreen.floatingButtonAddContact.isEnabled = true
-                self.mainScreen.floatingButtonAddContact.isHidden = false
+                
+                self.mainScreen.seeProfileButton.isEnabled = true
+                self.mainScreen.seeProfileButton.isHidden = false
+                self.mainScreen.seeProfileButton.setTitle("Profile", for: .normal)
+                self.mainScreen.seeProfileButton.addTarget(self, action: #selector(self.addSeeProfileButtonTapped), for: .touchUpInside)
+
                 
                 //MARK: Logout bar button...
                 self.setupRightBarButton(isLoggedin: true)
-                
-//                //MARK: Observe Firestore database to display the contacts list...
-//                self.database.collection("users")
-//                    .document((self.currentUser?.email)!)
-//                    .collection("contacts")
-//                    .addSnapshotListener(includeMetadataChanges: false, listener: {querySnapshot, error in
-//                        if let documents = querySnapshot?.documents{
-//                            self.contactsList.removeAll()
-//                            for document in documents{
-//                                do{
-//                                    let contact  = try document.data(as: Contact.self)
-//                                    self.contactsList.append(contact)
-//                                }catch{
-//                                    print(error)
-//                                }
-//                            }
-//                            self.contactsList.sort(by: {$0.name < $1.name})
-//                            self.mainScreen.tableViewContacts.reloadData()
-//                        }
-//                    })
                 
             }
         }
@@ -83,22 +63,12 @@ class ViewController: UIViewController {
         
         title = "Elephant Telehealth"
         
-        //MARK: patching table view delegate and data source...
-//        mainScreen.tableViewContacts.delegate = self
-//        mainScreen.tableViewContacts.dataSource = self
-        
-////        MARK: removing the separator line...
-//        mainScreen.tableViewContacts.separatorStyle = .none
-        
         //MARK: Make the titles look large...
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        //MARK: Put the floating button above all the views...
-        view.bringSubviewToFront(mainScreen.floatingButtonAddContact)
-        
-        //MARK: tapping the floating add contact button...
-        mainScreen.floatingButtonAddContact.addTarget(self, action: #selector(addContactButtonTapped), for: .touchUpInside)
-        mainScreen.seeProfileButton.addTarget(self, action: #selector(addSeeProfileButtonTapped), for: .touchUpInside)
+        self.mainScreen.seeProfileButton.isEnabled = false
+        self.mainScreen.seeProfileButton.isHidden = true
+
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -110,14 +80,36 @@ class ViewController: UIViewController {
         Auth.auth().signIn(withEmail: email, password: password)
     }
     
-    @objc func addContactButtonTapped(){
-        let addContactController = AddContactViewController()
-        addContactController.currentUser = self.currentUser
-        navigationController?.pushViewController(addContactController, animated: true)
-    }
-    
     @objc func addSeeProfileButtonTapped(){
+        self.showActivityIndicator()
+        Configs.myEmail = (currentUser?.email!.lowercased())!
+        let docDocuRef = self.database.collection("doctor").document(Configs.myEmail)
+        let patDocuRef = self.database.collection("patient").document(Configs.myEmail)
         
+        docDocuRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                //                        let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                let DLPScreen = DLPViewController()
+                self.hideActivityIndicator()
+                self.navigationController?.pushViewController(DLPScreen, animated: true)
+            } else {
+                patDocuRef.getDocument { (document, error) in
+                    if let document = document, document.exists {
+                        let plpScreen = PLPViewController()
+                        self.hideActivityIndicator()
+                        self.navigationController?.pushViewController(plpScreen, animated: true)
+                    } else {
+                        self.hideActivityIndicator()
+                        print("Document does not exist")
+                        //MARK: alert that patient data is missing
+                        let alert = UIAlertController(title: "Error!", message: "Error finding user data. Please contact support.", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .default))
+                        self.present(alert, animated: true)
+                    }
+                }
+                
+            }
+        }
     }
 }
 
